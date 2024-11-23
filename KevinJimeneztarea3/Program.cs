853fca551a8656b439e7b517e2cf06c39af639ae
Kevj702@outlook.com <Kevj702@outlook.com>
2024-11-22 21:39:40 -0400

    Declaración de variables

--- KevinJimeneztarea3/Program.cs
+++ KevinJimeneztarea3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double v1, v2, res;
+
+        int opt = 0;
+
+        do
+        {
+            Console.WriteLine("Calculadora online");
+            Console.WriteLine("Seleccione una opción");
+            Console.WriteLine("1.Sumar");
+            Console.WriteLine("2.Restar");
+            Console.WriteLine("3.Multiplicar");
+            Console.WriteLine("4.Dividir");
+            Console.WriteLine("5.Salir");
+            opt = int.Parse(Console.ReadLine());
+
+            switch(opt)
+            {
+                case 1:
+
+                   
+                    break;
+            }
+        } while ((opt>0) && (opt<=4));
+    }
+}
